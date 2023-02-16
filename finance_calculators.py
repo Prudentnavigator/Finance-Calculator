7d938cbf8312b019f3f76ab7ed882a3d2f3a0389
ThomasP <123633993+Prudentnavigator@users.noreply.github.com>
2023-02-16 10:19:55 +0100

    Update finance_calculators.py

--- finance_calculators.py
+++ finance_calculators.py
@@ -1,4 +1,4 @@
-Bootcamp Project II
+Bootcamp Project I
 
 # finance_calculators.py--a program that allows the user to access two different 
 #   financial calculators: an investment and a loan repayment calculator.
